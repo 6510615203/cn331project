b7c1429ba653b697f083d33ea8a7eb5eb8535d30
6510685040 <6510685040@student.tu.ac.th>
2024-11-16 22:55:25 +0700

    add file image

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from django.conf import settings
 from home import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("",views.index, name="index"),
@@ -11,3 +13,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('index_restaurant/', views.index_restaurant, name='index_restaurant')
 ]
+
+if settings.DEBUG:  # ใช้เฉพาะในโหมด debug (development server)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
