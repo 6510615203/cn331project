ffcb26ab61809dba7a374ff8bc1e2d588a642ec8
Ponthipa Teerapravet <6510615203@student.tu.ac.th>
2024-11-19 00:38:16 +0700

    edit models for restaurant registration

--- home/models.py
+++ home/models.py
@@ -30,6 +30,7 @@ class RestaurantProfile(models.Model):
     restaurant_name = models.CharField(max_length=100, blank=True)
     food_category =  models.CharField(max_length=100, blank=True)
     about = models.CharField(max_length=1000, blank=True)
+    open_close_time = models.CharField(max_length=100, blank=True)
     restaurant_picture = models.ImageField(upload_to='restaurant_picture/', null=True, blank=True)
 
     def __str__(self):
@@ -39,6 +40,8 @@ class Menu(models.Model):
     food_name = models.CharField(max_length=100, blank=True)
     food_category =  models.CharField(max_length=100, blank=True)
     about = models.CharField(max_length=1000, blank=True)
+    price = models.DecimalField(max_digits=10,     
+        decimal_places=2, default=0.00)
     menu_picture = models.ImageField(upload_to='menu_picture/', null=True, blank=True)
     def __str__(self):
         return self.food_name
