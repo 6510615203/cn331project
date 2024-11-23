eadb93fd7cc078818c018913df6b372286c97804
Ponthipa Teerapravet <6510615203@student.tu.ac.th>
2024-11-23 14:08:06 +0700

    create restaurant app

--- home/views.py
+++ home/views.py
@@ -29,12 +29,14 @@ def order(request):
 def yourorder(request):      
     return render(request, "index.html")
 
+"""
 def index_restaurant(request):
-    """
+    
     if request.method == "POST":
         return HttpResponseRedirect("index_restaurant")
-    """   
+ 
     return render(request, "index_restaurant.html")
+"""
 
 def welcome_registration(request): 
     user_type = request.GET.get('user_type', 'default_value') 
@@ -209,7 +211,7 @@ def login_view(request):
                 user_type = None
 
             if user_type == "restaurant":
-                return redirect("index_restaurant")  
+                return redirect("/restaurant/")  
             else:
                 return redirect("index") 
 
