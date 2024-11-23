976e345fcf41fd66df27617cada9e4cc5ea76509
Ponthipa Teerapravet <6510615203@student.tu.ac.th>
2024-11-23 14:08:06 +0700

    edit navigator for restaurant

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = "restaurant"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('manage', views.manage, name='manage'),
+    path('about_kinkorn', views.about, name='about'),
+    path('order_list', views.order_list, name='order_list'),
+    path('sales_report', views.sales_report, name='sales_report')
 ]
